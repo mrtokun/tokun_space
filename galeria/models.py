fbdb440288bb85573711db20630f28764ce8df5b
mrtokun <tokunaga.paulo3@gmail.com>
2024-06-20 20:49:19 -0300

    Adicionando uso de models

--- galeria/models.py
+++ galeria/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Fotografia(models.Model):
+    nome = models.CharField(max_length=100, null=False, blank=False)
+    legenda = models.CharField(max_length=150, null=False, blank=False)
+    descricao = models.TextField(null=False, blank=False)
+    foto = models.CharField(max_length=100, null=False, blank=False)
+
+    def __str__(self):
+        # Um exemplo de utilizacao disso é no manage.py shell 
+        # from galeria.models import Fotografia
+        # Fotografia.objects.all() que se utilizará do método __str__
+        return f"Fotografia [nome={self.nome}]"
--- galeria/views.py
+++ galeria/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 
+
 def index(request):
+
+    dados = {
+        1: {"nome": "Nebulosa de Carina",
+            "legenda": "webbtelescope.org / NASA / James Webb"},
+        2: {"nome": "Galaxia NGC 1079",
+            "legenda": "nasa.org / NASA / Hubble"}
+    }
+
     # return HttpResponse('<h1>Tokun Space </h1><p>Bem-vindo ao espaço do tokun</p>')
-    return render(request, 'galeria/index.html')
+    return render(request, 'galeria/index.html', {"cards2": dados})
 
 def imagem(request):
     return render(request, 'galeria/imagem.html')
